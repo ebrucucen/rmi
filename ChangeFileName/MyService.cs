80bed8d3f06313435ac817f2be85392251d57f7d
Ebru Cucen <ebrugul@hotmail.com>
2016-04-07 14:32:55 +0100

    basic created/delete file windows service

---
+++ ChangeFileName/MyService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ChangeFileName
+{
+    public partial class MyService : ServiceBase
+    {
+        public MyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            
+          var result= CreateCopyFile();
+        }
+        static bool CreateCopyFile()
+        {
+            var datePreStamp = DateTime.Now.AddMinutes(-1).ToString("ddMMyy_hhmm");
+            var dateNowstamp = DateTime.Now.ToString("ddMMYY_hhmm");
+            var file = @"c:\tempa" + dateNowstamp.ToString() + ".txt";
+            var preFile= @"c:\tempa" + datePreStamp.ToString() + ".txt";
+            // exceptions:
+            if (System.IO.File.Exists(file))
+            {
+                throw new Exception("the file already exists");
+            }else if (!(File.Exists(preFile)))
+            {
+                throw new Exception("the pre file exists");
+            }
+            else
+            {
+                File.Create(file);
+                File.Delete(preFile);
+            }
+         return true;
+        }
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ RMI.RefCheckService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMI.RefCheckService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run()
+            var result=CreateFileCopy;
+        }
+       
+    }
+    private System.ComponentModel.IContainer components;
+    private System.Diagnostics.EventLog eventLog1;
+
+    public MyNewService()
+    {
+        InitializeComponent();
+        var eventLog1 = new System.Diagnostics.EventLog();
+        if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+        {
+            System.Diagnostics.EventLog.CreateEventSource(
+                "MySource", "MyNewLog");
+        }
+        eventLog1.Source = "MySource";
+        eventLog1.Log = "MyNewLog";
+    }
+
+}
