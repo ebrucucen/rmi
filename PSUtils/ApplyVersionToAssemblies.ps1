66a6f42f2ee777b70f5c99d9dc4d315fd740b9ec
Ebru Cucen <ebrugul@hotmail.com>
2016-04-08 10:40:31 +0100

    assmblyversion changes

--- PSUtils/ApplyVersionToAssemblies.ps1
+++ PSUtils/ApplyVersionToAssemblies.ps1
@@ -2,37 +2,46 @@
 # ApplyVersionToAssemblies.ps1
 #
 
-[CmdletBinding()]
+function Convert-DateString ([String]$Date, [String[]]$Format)
+{
+   $result = New-Object DateTime
+ 
+   $convertible = [DateTime]::TryParseExact(
+      $Date,
+      $Format,
+      [System.Globalization.CultureInfo]::InvariantCulture,
+      [System.Globalization.DateTimeStyles]::None,
+      [ref]$result)
+ 
+   if ($convertible) { $result }
+}
+function ApplyVersion(){
 param(
 [Parameter(Mandatory=$True, Position=1)]
 [string]$sourcesDirectory,
 [Parameter(Mandatory=$True, Position=2)]
-[string]$buildNumber)
+[string]$buildNumber,
+[Parameter(Mandatory=$True, Position=3)]
+[string]$MajorMinor
+)
 
-$versionRegex= "\d+\.\d+"
-$versionData= [regex]::Matches($buildNumber, $versionRegex)
+$buildDate= [regex]::Matches($buildNumber, "\d+")[0].value
+$revisionNumber=  [regex]::Matches($buildNumber, "\d+")[1].value
+
+$startdate=[datetime] ($buildDate.substring(0,4) + "/01/01 00:00:00")
+$enddate= convert-datestring -date $buildDate -Format "yyyyMMdd"
+$elapsedDays=(NEW-TIMESPAN –Start $StartDate –End $EndDate).Days
+$versionRegex= "\d+\.\d+\.\d+\.d+"
+#$versionData= [regex]::Matches($buildNumber, $versionRegex)
+$versionData= $MajorMinor + "." + $elapsedDays + "." + $revisionNumber
+
 Write-Verbose -Verbose "buildNumber:$buildNumber"
 Write-Verbose -Verbose "versionRegex:$versionRegex"
 Write-Verbose -Verbose "versionData.Count:$($versionData.Count)"
 Write-Verbose -Verbose "versionData:$($versionData.Tostring())"
 
 
-switch ($versionData.Count)
-{
-	0
-	{
-		Write-Error "Could not find version number in $buildNumber"
-	}
-	1{}
-	default
-	{
-		Write-Warning "found more than intance of data in buildnumber"
-		Write-Warning "will assume first instance is version"
-	}
-}
-$newVersion= $versionData[0]
-Write-Verbose -Verbose "Version:$newVersion"
-
+Writ
 $files= gci $sourcesDirectory -Recurse -Include "*Properties*" |?{$_.PSIscontainer}| foreach {
 	gci -Path $_.FullName -Recurse -include AssemblyInfo.*}
 if($files)
@@ -41,10 +50,12 @@ if($files)
 	foreach($file in $files){
 		$fileContent= Get-Content ($file)
 		attrib $file -r
-		$fileContent -replace $versionRegex, $newVersion | Out-File $file
+		$fileContent -replace $versionRegex, $versionData | Out-File $file
 		Write-Verbose -Verbose "$file.FullName - version applied" 
 	}
 }
 else{
 	Write-Warning -Verbose "Found no files."
 }
+}
+Apply-Version $sourcesDirectory $buildNumber
