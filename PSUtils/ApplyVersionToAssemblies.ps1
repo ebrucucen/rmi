391a67716c8931690aff09221edfd01ddd88502d
Ebru Cucen <ebrugul@hotmail.com>
2016-04-08 09:36:35 +0100

    ApplyVersionToAssemblies added

---
+++ PSUtils/ApplyVersionToAssemblies.ps1
@@ -0,0 +1,44 @@
+﻿#
+# ApplyVersionToAssemblies.ps1
+#
+
+[CmdletBinding()]
+param(
+[Parameter(Mandatory=$True, Position=1)]
+[string]$sourcesDirectory,
+[Parameter(Madatory=$True, Position=2)]
+[string]$buildNumber)
+
+$versionRegex= "\d+\.\d+\.\d+\.\d+"
+$versionData= [regex]::Matches($buildNumber, $versionRegex)
+switch ($versionData.Count)
+{
+	0
+	{
+		Write-Error "Could not find version number in $buildNumber"
+	}
+	1{}
+	default
+	{
+		Write-Warning "found more than intance of data in buildnumber"
+		Write-Warning "will assume first instance is version"
+	}
+}
+$newVersion= $versionData[0]
+Write-Verbose -Verbose "Version:$newVersion"
+
+$files= gci $sourcesDirectory -Recurse -Include "*Properties*" |?{$_.PSIscontainer}| foreach {
+	gci -Path $_.FullName -Recurse -include AssemblyInfo.*}
+if($files)
+{
+	Write-Verbose -Verbose "Will apply $newVersion to $($files.Count) files"
+	foreach($file in $files){
+		$fileContent= Get-Content ($file)
+		attrib $file -r
+		$fileContent -replace $versionRegex, $newVersion | Out-File $file
+		Write-Verbose -Verbose "$file.FullName - version applied" 
+	}
+}
+else{
+	Write-Warning -Verbose "Found no files."
+}
